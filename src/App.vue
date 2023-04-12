fe5fd39b5ec9765cbf967b5940ba22fe1cca3eef
lynoting <kawanosakiko@gmail.com>
2023-04-12 09:59:10 +0900

    0412

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,25 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
-</template>
-
-<script>
-import HelloWorld from './components/HelloWorld.vue'
-
-export default {
+  <div>
+    <AskGPT />
+  </div>
+ </template>
+ <script>
+ import AskGPT from './components/AskGPT.vue'
+  
+ export default {
   name: 'App',
   components: {
-    HelloWorld
+    AskGPT
   }
-}
-</script>
-
-<style>
-#app {
+ }
+ </script>
+ <style>
+ #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
-}
-</style>
+ }
+ </style>
---
+++ src/components/AskGPT.vue
@@ -0,0 +1,104 @@
+<template>
+  <div>
+    <header>
+      <h1 class="mb-3">Genron SF - ask to GPT</h1>
+    </header>
+    <br />
+    <div class="mb-3 form-group">
+      <div class="container-fluid">
+        <div class="row">
+          <div class="col-md-1"></div>
+          <div class="col-md-10">
+            <form>
+              <div class="form-group">
+                <label for="exampleFormControlInput1" class="form-label"
+                  >Input your Genron SF work URL</label
+                >
+                <input
+                  type="url"
+                  class="form-control"
+                  id="exampleFormControlInput1"
+                  placeholder="https://school.genron.co.jp/works/sf/2020/students/skawano/4486/"
+                />
+              </div>
+              <br />
+              <button type="button" class="btn btn-outline-primary">
+                Start!
+              </button>
+            </form>
+          </div>
+          <div class="col-md-1"></div>
+        </div>
+      </div>
+    </div>
+    <div class="mb-3 form-group">
+      <div class="container-fluid">
+        <div class="row">
+          <div class="col-md-1"></div>
+          <div class="col-md-10">
+            <form>
+              <div class="form-group">
+                <label for="exampleFormControlInput1" class="form-label">
+                  タイトルへのコメント
+                </label>
+                <textarea
+                  class="form-control"
+                  id="exampleFormControlTextarea1"
+                  rows="3"
+                ></textarea>
+              </div>
+            </form>
+            <br />
+            <form>
+              <div class="form-group">
+                <label for="exampleFormControlInput1" class="form-label">
+                  梗概へのコメント
+                </label>
+                <textarea
+                  class="form-control"
+                  id="exampleFormControlTextarea1"
+                  rows="3"
+                ></textarea>
+              </div>
+            </form>
+            <br />
+            <form>
+              <div class="form-group">
+                <label for="exampleFormControlInput1" class="form-label">
+                  実作の冒頭部へのコメント
+                </label>
+                <textarea
+                  class="form-control"
+                  id="exampleFormControlTextarea1"
+                  rows="3"
+                ></textarea>
+              </div>
+            </form>
+          </div>
+          <div class="col-md-1"></div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<!-- <script lang="ts">
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  setup() {
+    
+    let titleComment = "";
+    
+    const askTitleComment = () => titleComment = "";
+
+    return {
+        titleComment,
+        askTitleComment,
+    };
+  },
+});
+</script> -->
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped></style>
